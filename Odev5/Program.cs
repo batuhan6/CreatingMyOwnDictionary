e0c30f5a76bf4cf2665bbe2d129748c8970c7b3d
batuhan6 <32600613+batuhan6@users.noreply.github.com>
2021-05-23 17:01:57 +0300

    Add project files.

---
+++ Odev5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary_ödev5
+{
+    class Program
+    {
+        private static Dictionary<string, object> dict;
+
+        private static void Add(string strKey, object dataType)
+        {
+            if (!dict.ContainsKey(strKey))
+            {
+                dict.Add(strKey, dataType);
+            }
+            else
+            {
+                dict[strKey] = dataType;
+            }
+        }
+
+        private static T GetAnyValue<T>(string strKey)
+        {
+            object obj;
+            T retType;
+
+            dict.TryGetValue(strKey, out obj);
+
+            try
+            {
+                retType = (T)obj;
+            }
+            catch
+            {
+                retType = default(T);
+            }
+            return retType;
+        }
+
+        static void Main(string[] args)
+        {
+            dict = new Dictionary<string, object>();
+
+            Add("pi", 3.14);
+            Add("dongu", "Round and round");
+            Console.WriteLine(GetAnyValue<double>("pi"));
+            Console.WriteLine(GetAnyValue<string>("dongu"));
+            Console.ReadLine();
+        }
+    }
+
+
+}
+
